dd7591c0462cb67358895194006014555c80b143
Vitalii Martynov <vitalii.martynov@sixt.com>
2025-11-20 22:15:25 -0400

    Initial solution

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,77 @@
-export const Pagination = () => {};
+import React from 'react';
+import cn from 'classnames';
+import { getNumbers } from '../../utils';
+
+type Props = {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+};
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const totalPages = Math.ceil(total / perPage);
+  const pages = getNumbers(1, totalPages).map(n => n);
+  const isFirst = currentPage === 1;
+  const isLast = currentPage === totalPages;
+
+  return (
+    <ul className="pagination">
+      <li className={`page-item ${isFirst ? 'disabled' : ''}`}>
+        <a
+          data-cy="prevLink"
+          className="page-link"
+          href="#prev"
+          aria-disabled={isFirst}
+          onClick={e => {
+            e.preventDefault();
+            if (!isFirst) {
+              onPageChange(currentPage - 1);
+            }
+          }}
+        >
+          «
+        </a>
+      </li>
+      {pages?.slice(0, totalPages).map(page => (
+        <li
+          className={cn('page-item', {
+            active: currentPage === page,
+          })}
+          data-cy="page-item"
+          key={page}
+        >
+          <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${page}`}
+            onClick={() => onPageChange(page)}
+          >
+            {page}
+          </a>
+        </li>
+      ))}
+      <li className={`page-item ${isLast ? 'disabled' : ''}`}>
+        <a
+          data-cy="nextLink"
+          className="page-link"
+          href="#next"
+          aria-disabled={isLast}
+          onClick={e => {
+            e.preventDefault();
+            if (!isLast) {
+              onPageChange(currentPage + 1);
+            }
+          }}
+        >
+          »
+        </a>
+      </li>
+    </ul>
+  );
+};
